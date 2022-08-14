3c9d21b9c566a53423125efd9bd69c2a51b36d7c
Daniel Esuola <esuoladaniel002@gmail.com>
2022-08-14 16:33:15 +0100

    connecting API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import axios from "axios";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Col } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
